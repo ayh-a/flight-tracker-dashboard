3a7b283e04fa83a259d095c7e277663bd714826b
Ayhan A <ayhan.ahsan@gmail.com>
2025-03-14 14:59:27 -0400

    created service for openskyapi and created .yml

---
+++ DashboardAPI/src/main/java/com/ayh/DashboardAPI/service/OpenSkyApiService.java
@@ -0,0 +1,26 @@
+package com.ayh.DashboardAPI.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OpenSkyApiService implements ApiService {
+    @Value("${apis.opensky.username")
+    private String username;
+
+    @Value("${apis.opensky.password}")
+    private String password;
+
+//    public Object getAllStates() {
+//      TODO: implement
+//    }
+
+//    public Object GetAllStatesInBoundingBox() {
+//      TODO: implement
+//    }
+
+    @Override
+    public String getName() {
+        return "opensky";
+    }
+}
