363d059e348aed445cf83f126a7363f2b9ebaa7e
Ayhan A <ayhan.ahsan@gmail.com>
2025-03-14 04:05:00 -0400

    initial commit

---
+++ DashboardAPI/src/test/java/com/ayh/DashboardAPI/DashboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayh.DashboardAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
