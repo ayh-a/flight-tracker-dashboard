7101edbc333e289414d30a28cef48b8353c303de
Ayhan A <ayhan.ahsan@gmail.com>
2025-03-14 23:59:17 -0400

    update gitignore for gradle

--- DashboardAPI/src/main/java/com/ayh/DashboardAPI/service/OpenSkyApiService.java
+++ DashboardAPI/src/main/java/com/ayh/DashboardAPI/service/OpenSkyApiService.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class OpenSkyApiService implements ApiService {
-    @Value("${apis.opensky.username")
-    private String username;
-
-    @Value("${apis.opensky.password}")
-    private String password;
+//    @Value("${apis.opensky.username")
+//    private String username;
+//
+//    @Value("${apis.opensky.password}")
+//    private String password;
 
 //    public Object getAllStates() {
 //      TODO: implement
