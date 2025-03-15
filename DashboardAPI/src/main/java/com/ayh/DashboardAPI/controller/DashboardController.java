4235af12886242b4019e4b86538133d36bbbb8af
Ayhan A <ayhan.ahsan@gmail.com>
2025-03-15 02:19:55 -0400

    get system variables for api creds; working

--- DashboardAPI/src/main/java/com/ayh/DashboardAPI/controller/DashboardController.java
+++ DashboardAPI/src/main/java/com/ayh/DashboardAPI/controller/DashboardController.java
@@ -1,5 +1,7 @@
 package com.ayh.DashboardAPI.controller;
 
+import com.ayh.DashboardAPI.service.OpenSkyApiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
