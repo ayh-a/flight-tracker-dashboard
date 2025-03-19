22af93d7f9f3ed436a6fddf60c2c976bd18875ac
Ayhan A <ayhan.ahsan@gmail.com>
2025-03-19 03:10:11 -0400

    weather webclient config and initial weatherApiService impl

--- DashboardAPI/src/main/java/com/ayh/DashboardAPI/service/WeatherApiService.java
+++ DashboardAPI/src/main/java/com/ayh/DashboardAPI/service/WeatherApiService.java
@@ -1,11 +1,31 @@
 package com.ayh.DashboardAPI.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+@Service
 public class WeatherApiService implements ApiService{
 
-    WeatherApiService(@Value("${apis.weather.key}") String apiKey) {
+    private final WebClient weatherWebClient;
 
+    @Value("${apis.weather.key}")
+    private String apiKey;
+
+    WeatherApiService(WebClient weatherWebClient) {
+        this.weatherWebClient = weatherWebClient;
+    }
+
+    public Mono<String> getWeather(double lat, double lon) {
+        return weatherWebClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("lat", lat)
+                        .queryParam("lon", lon)
+                        .queryParam("key", apiKey)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class);
     }
 
 
