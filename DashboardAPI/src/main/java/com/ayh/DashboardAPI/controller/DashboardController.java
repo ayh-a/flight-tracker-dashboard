1a3e0791e96a50a9f61555cc5f8bf84b3e3c6acf
Ayhan A <ayhan.ahsan@gmail.com>
2025-03-25 08:29:15 -0400

    cors

--- DashboardAPI/src/main/java/com/ayh/DashboardAPI/controller/DashboardController.java
+++ DashboardAPI/src/main/java/com/ayh/DashboardAPI/controller/DashboardController.java
@@ -3,12 +3,10 @@ package com.ayh.DashboardAPI.controller;
 import com.ayh.DashboardAPI.dto.DashboardSummaryDTO;
 import com.ayh.DashboardAPI.dto.FlightsInBoundsDTO;
 import com.ayh.DashboardAPI.service.DashboardService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class DashboardController {
     DashboardService dashboardService;
